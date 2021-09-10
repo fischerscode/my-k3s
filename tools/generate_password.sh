4d29f64a8cc2b4b5ba133881c7e5a6b27aa83b49
Maximilian Fischer <github@maaeps.de>
2021-09-10 18:07:40 +0200

    don't use quotes in passwords

--- tools/generate_password.sh
+++ tools/generate_password.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-LC_ALL=C tr -dc 'A-Za-z0-9!"#$%&'\''()*+,-./:;<=>?@[\]^_`{|}~' </dev/urandom | head -c $1 ; echo
+LC_ALL=C tr -dc 'A-Za-z0-9!#$%&()*+,-./:;<=>?@[\]^_`{|}~' </dev/urandom | head -c $1 ; echo
