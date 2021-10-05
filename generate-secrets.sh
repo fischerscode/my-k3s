794aa121654db8d94f8932dda0b05e26d6c0ee73
Maximilian Fischer <github@maaeps.de>
2021-10-05 17:21:25 +0200

    Update generate-secrets.sh

--- generate-secrets.sh
+++ generate-secrets.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+echo -n "Docker username: "
+read dockerUsername
+echo -n "Docker password or access token: "
+read -s dockerPassword
+kubectl create secret docker-registry docker-cred --docker-server=https://index.docker.io/v1/ --docker-username=$dockerUsername --docker-password=$dockerPassword --dry-run=client -o yaml > infrastructure/$CLUSTER/docker-cred.yaml; sops --encrypt --in-place infrastructure/$CLUSTER/docker-cred.yaml
+
 echo "metallb:"
 kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(tools/generate_password.sh 128)" --dry-run=client -o yaml > infrastructure/$CLUSTER/metallb/memberlist.yaml; sops --encrypt --in-place infrastructure/$CLUSTER/metallb/memberlist.yaml
 
@@ -12,9 +19,9 @@ tools/generate_password.sh 128 > infrastructure/$CLUSTER/pdns/pdns-admin-secret-
 docker run --rm -ti python /bin/bash -c "pip3 -q install bcrypt 2> /dev/null && python -c 'import bcrypt; print(bcrypt.gensalt().decode(), end = \"\")'" > infrastructure/$CLUSTER/pdns/pdns-admin-salt.encrypted; sops --encrypt --in-place infrastructure/$CLUSTER/pdns/pdns-admin-salt.encrypted
 
 echo "Monitoring:"
-echo "Slack webhook for monitoring:"; read webHook; printf $webHook > infrastructure/$CLUSTER/monitoring/slack-webhook.encrypted; sops --encrypt --in-place infrastructure/$CLUSTER/monitoring/slack-webhook.encrypted
+echo -n "Slack webhook for monitoring: "; read webHook; printf $webHook > infrastructure/$CLUSTER/monitoring/slack-webhook.encrypted; sops --encrypt --in-place infrastructure/$CLUSTER/monitoring/slack-webhook.encrypted
 tools/generate_password.sh 128 > infrastructure/$CLUSTER/monitoring/grafana-admin-password.encrypted; sops --encrypt --in-place infrastructure/$CLUSTER/monitoring/grafana-admin-password.encrypted
 
 echo "Traefik:"
-echo "Traefik pilot token"; read token; printf $token > infrastructure/$CLUSTER/traefik/pilot-token.encrypted; sops --encrypt --in-place infrastructure/$CLUSTER/traefik/pilot-token.encrypted
-echo "Traefik dashboard admin password"; htpasswd -nB admin > infrastructure/$CLUSTER/traefik/traefik-dashboard-users.txt
+echo -n "Traefik pilot token: "; read token; printf $token > infrastructure/$CLUSTER/traefik/pilot-token.encrypted; sops --encrypt --in-place infrastructure/$CLUSTER/traefik/pilot-token.encrypted
+echo "Traefik dashboard admin password:"; htpasswd -nB admin > infrastructure/$CLUSTER/traefik/traefik-dashboard-users.txt
